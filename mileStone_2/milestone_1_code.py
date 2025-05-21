66dda28e67f6ac8b7bc0141bae8942c4f9f19f02
Dharmaip <dharmaip@wsc.school.nz>
2025-05-21 15:13:06 +1200

    Update milestone_1_code.py

--- mileStone_2/milestone_1_code.py
+++ mileStone_2/milestone_1_code.py
@@ -1,11 +1,15 @@
 name = input("What is your name?")
 age = int(input("How old are you?"))
+#Below is executed only if the user age is equal to or under 5
 if age <= 5:
     recommended_sleep = "10 - 12 hours per night"
+#Below is executed only if the user age is equal to or under 18
 elif age <= 18:
     recommended_sleep = "7 - 9 hours per night"
+#Below is executed only if the user age is equal to or under 30
 elif age <= 30:
     recommended_sleep = "6 - 8 hours per night"
+#Below is executed only if none of the above conditions are met
 else:
     recommended_sleep = "7 - 8 hours per night"
-print("Hello {}! your age is {} and I recommend you to sleep at {}".format(name.title(), age, recommended_sleep))
+print("Hello {}! Since you are {} years old, I recommend you to sleep at {} for better health".format(name.title(), age, recommended_sleep))
